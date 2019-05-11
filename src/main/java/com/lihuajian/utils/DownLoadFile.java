60ee3cc20eec55f9ea51598769ed60f2c9b02b08
lihuajian317 <1649077012@qq.com>
2019-05-11 11:24:09 +0800

    下载服务器文件到本地

---
+++ src/main/java/com/lihuajian/utils/DownLoadFile.java
@@ -0,0 +1,66 @@
+package com.lihuajian.utils;
+
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownLoadFile {
+	/**  
+     * 下载远程文件并保存到本地  
+     * @param remoteFilePath 远程文件路径   
+     * @param localFilePath 本地文件路径  
+     */
+    public static void downloadFile(String remoteFilePath, String localFilePath)
+    {
+        URL urlfile = null;
+        HttpURLConnection httpUrl = null;
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        File f = new File(localFilePath);
+        try
+        {
+        	if(f.isFile()){
+        		File ff=f.getParentFile();
+        		ff.mkdirs();
+        	}
+        	if(!f.exists()){
+        		File ff=f.getParentFile();
+        		ff.mkdirs();
+        		
+        	}
+        	f.createNewFile();
+            urlfile = new URL(remoteFilePath);
+            httpUrl = (HttpURLConnection)urlfile.openConnection();
+            httpUrl.connect();
+            bis = new BufferedInputStream(httpUrl.getInputStream());
+            bos = new BufferedOutputStream(new FileOutputStream(f));
+            int len = 2048;
+            byte[] b = new byte[len];
+            while ((len = bis.read(b)) != -1)
+            {
+                bos.write(b, 0, len);
+            }
+            bos.flush();
+            bis.close();
+            httpUrl.disconnect();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                bis.close();
+                bos.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+   
+}
